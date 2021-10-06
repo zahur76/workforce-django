8795c67c842b349bf944e4a184b4be9e06cd3ad8
zahur <zahurmeerun@hotmail.com>
2021-10-06 12:50:58 +0400

    create view to login

--- login/views.py
+++ login/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+# login view
+def login_view(request):
+    ''' View to login as admin '''    
+    username = request.POST['username']    
+    password = request.POST['password']    
+    user = authenticate(request, username=username, password=password)
+    if user is not None:                 
+        login(request, user)            
+        messages.success(request, 'Login Successful!')
+        return redirect(reverse('home'))
+    else:
+        # Return an 'invalid login' error message.        
+        messages.error(request, 'Login Unsuccessful!')
+        return redirect(reverse('home'))
