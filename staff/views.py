23db8d9c405837a2a3e094b6644d09cc21444646
zahur <zahurmeerun@hotmail.com>
2021-10-07 20:25:49 +0400

    add view to show staff employed

--- staff/views.py
+++ staff/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from .models import Staff
 
 
 # Create your views here
@@ -8,6 +9,11 @@ def staff(request):
     if not request.user.is_superuser:
         messages.error(request, 'Permision Denied!.')
         return redirect(reverse('home')) 
+    else:
+        staff = Staff.objects.all()
 
+        context = {
+            'staff': staff,
+        }
     
-    return render(request, 'staff/staff.html')
+    return render(request, 'staff/staff.html', context)
