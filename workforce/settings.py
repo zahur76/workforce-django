4baaa2c3597f86dc73941bd93a1f3e5797c7c801
zahur <zahurmeerun@hotmail.com>
2021-10-15 11:22:52 +0400

    change allowed host for heroku app

--- workforce/settings.py
+++ workforce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['django-starwars.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['workforce-django.herokuapp.com/', 'localhost', '127.0.0.1']
 
 
 # Application definition
