709cb21239fee94d286cf7d567524b8fe6ea6123
zahur <zahurmeerun@hotmail.com>
2021-11-25 08:54:29 +0400

    add salary url added

--- pay_management/urls.py
+++ pay_management/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [    
+urlpatterns = [
     path('', views.pay, name='pay'),
+    path('add_salary/<int:integer>', views.add_salary, name='add_salary'),
 ]
