8c769adb3698b9dcebd28a26f9f2d805879e79ca
zahur <zahurmeerun@hotmail.com>
2021-12-01 08:43:06 +0400

    add view salary modal

--- pay_management/static/pay_management/js/pay.js
+++ pay_management/static/pay_management/js/pay.js
@@ -2,13 +2,19 @@ $(document).ready(function(){
 
     $(".salary-deletion-modal").click(function(){        
         let deleteSalaryModal = $(this).attr("id").split("-")[4]
-        let openDeletionMOdal = `#delete-salary-modal-${deleteSalaryModal}`
-        $(openDeletionMOdal).show(); 
+        let openDeletionModal = `#delete-salary-modal-${deleteSalaryModal}`
+        $(openDeletionModal).show(); 
     })
 
+    $(".view-salary-modal").click(function(){        
+        let openSalaryModal = $(this).attr("id").split("-")[4]
+        let viewSalaryModal = `#view-salary-modal-${openSalaryModal}`
+        $(viewSalaryModal).show();  
+    })
 
     $(".close-button").click(function(){       
-        $(".confirm-modal").hide(); 
+        $(".confirm-modal").hide();
+        $(".view-modal").hide(); 
     })    
 
 });
