ee67ec783e6225023d14a9594a1890a58669e465
zahur <zahurmeerun@hotmail.com>
2021-12-04 11:40:35 +0400

    install pscopg2

--- pay_management/util.py
+++ pay_management/util.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
-from 
+from xhtml2pdf import pisa  
  
 def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
